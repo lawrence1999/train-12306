af19897a909f672cb8685f0f249c535c00875cc4
71300 <741300979@qq.com>
2024-04-08 23:29:42 +0800

    feat: initialize member/common repo

---
+++ member/src/main/java/org/example/train/MemberApplication.java
@@ -0,0 +1,24 @@
+package org.example.train;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+
+/**
+ * Description:
+ * Date: 2024/4/8
+ * Author: lawrence
+ */
+@SpringBootApplication
+public class MemberApplication {
+    public static final Logger log = LoggerFactory.getLogger(MemberApplication.class);
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(MemberApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+        log.info("MemberApplication start success,http://127.0.0.1:{}", environment.getProperty("server.port"));
+
+    }
+}
