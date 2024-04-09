a2b0d0ca624c6683f597336b0e0eafd62494479d
renyu.liu <renyu.liu@cri-united-imaging.com>
2024-04-09 15:39:11 +0800

    feat:增加gateway模块，可以转发到member

---
+++ gateway/src/main/java/org/example/gateway/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.example.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Description:
+ * Date: 2024/4/8
+ * Author: lawrence
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello111";
+    }
+}
